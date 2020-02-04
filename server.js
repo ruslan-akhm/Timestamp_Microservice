5bdcd6ba2d7b1bdef7a8b3924dd017a04b946bfc
ruslan-akhm <47538460+ruslan-akhm@users.noreply.github.com>
2020-02-04 19:28:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,31 +9,33 @@ app.get("/api/timestamp/:date_string?",(req,res)=>{
   const dash = /[-]/;
   //console.log("string"+new Date(date_string))
   //console.log(new Date(date_string).toUTCString())
-  //console.log("integer"+new Date(int_date))
+  //console.log("string"+new Date(int_date))
   //console.log(new Date(int_date).toUTCString())
   
-  if(typeof date_string=="undefined"||date_string.length<1){ //if empty req; Should check for empty request first  !!
+  //Checking for empty string in a req
+  if(!date_string && isNaN(int_date)/*typeof date_string=="undefined"||date_string.length<1*/){ 
     console.log("Empty req was received")
-    const emptyUnix=Date.now();
+    const emptyUnix=new Date().getTime();
     const emptyUtc=new Date().toUTCString();
     res.json({"unix":emptyUnix,"utc":emptyUtc});
     return;
   };
-  
-  if(new Date(date_string)=="Invalid Date" && new Date(int_date)=="Invalid Date"){ //If date is invalid e.g. tdew5fsd
+  //Checking If date is invalid e.g. tdew5fsd
+  if(new Date(date_string)=="Invalid Date" && date_string.length!==int_date.length && new Date(int_date)=="Invalid Date"){ 
     res.json({"error" : "Invalid Date"});
     return;
   };
   
-  
-  if(dash.test(date_string) && new Date(date_string).toUTCString()!=="Invalid Date"){// e.g. 2016-10-10
+  //Checking for ISO format e.g. 2016-10-10
+  if(dash.test(date_string) && new Date(date_string).toUTCString()!=="Invalid Date"){
     console.log("yyyy-mm-dd req was received")
     const dateUnix=Date.parse(date_string);
     const dateUtc=new Date(date_string).toUTCString();
     res.json({"unix":dateUnix,"utc":dateUtc});
     return;
   }
-  if(new Date(int_date)!=="Invalid Date"){//unix was received
+  //Checking if unix was received
+  if(new Date(int_date)!=="Invalid Date"){
     console.log("unix was received")
     res.json({"unix":int_date,"utc":new Date(int_date).toUTCString()})
     return
