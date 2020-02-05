0e89539a56df850beec86fce96a19fc786eeef32
ruslan-akhm <47538460+ruslan-akhm@users.noreply.github.com>
2020-02-04 23:47:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.get("/api/timestamp/:date_string?",(req,res)=>{
   const date_string = req.params.date_string;
   const int_date = parseInt(date_string); // Have req as integer stored in here
   const dash = /[-]/;
-  //console.log("string"+new Date(date_string))
+  console.log("string"+new Date(date_string))
   //console.log(new Date(date_string).toUTCString())
-  //console.log("string"+new Date(int_date))
+  console.log("string"+new Date(int_date))
   //console.log(new Date(int_date).toUTCString())
   
   //Checking for empty string in a req
@@ -21,7 +21,7 @@ app.get("/api/timestamp/:date_string?",(req,res)=>{
     return;
   };
   //Checking If date is invalid e.g. tdew5fsd
-  if(new Date(date_string)=="Invalid Date" && date_string.length!==int_date.length && new Date(int_date)=="Invalid Date"){ 
+  if((new Date(date_string)=="Invalid Date" && date_string.length!==int_date.length) || new Date(int_date)=="Invalid Date"){ 
     res.json({"error" : "Invalid Date"});
     return;
   };
